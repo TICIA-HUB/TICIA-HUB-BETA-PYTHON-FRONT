e6ebce6a6f0f1779639742fe328ba2112b05c22a
Lucasikawa <lucasbqi@al.insper.edu.br>
2024-05-07 14:49:45 -0300

    confio

--- pages/lista_clientes.py
+++ pages/lista_clientes.py
@@ -5,7 +5,7 @@ import pandas as pd
 def show():
     st.header("Lista de Clientes")
 
-    clientes = requests.get("http://127.0.0.1:5000/clientes")
+    clientes = requests.get("https://ticiahub-6e8cd092033f.herokuapp.com/clientes")
 
     try:
         clientes = pd.DataFrame(clientes.json()['clientes'], columns=['id', 'nome', 'sobrenome', 'email', 'whatsapp', 'data_nascimento', 'cep'])
